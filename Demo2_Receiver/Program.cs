dfa2b93412814b84a3924d7b9f71d92772b458cd
Jim Fasenmyer <jfasenmyer@gmail.com>
2014-06-15 08:54:11 -0400

    Initial Commit

---
+++ Demo2_Receiver/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Demo2_Receiver
+{
+    class Program
+    {
+        private readonly static dynamic RabbitConfiguration = new
+        {
+            Server = "192.168.100.86",
+            Username = "jfasenmyer",
+            Password = "password",
+            QueueName = "hello"
+        };
+
+        public static IModel BuildChannel(string hostName, string username, string password, int port = 5672)
+        {
+            ConnectionFactory factory = new ConnectionFactory { HostName = hostName, Port = port, UserName = username, Password = password };
+
+            IConnection connection = factory.CreateConnection();
+
+            return connection.CreateModel();
+        }
+
+        static void Main(string[] args)
+        {
+            // Build a new channel to Rabbit.
+            IModel channel = BuildChannel(RabbitConfiguration.Server, RabbitConfiguration.Username, RabbitConfiguration.Password);
+
+            channel.QueueDeclare(RabbitConfiguration.QueueName, false, false, false, null);
+
+            //const bool noAck = true;
+
+            const bool noAck = false;
+            channel.BasicQos(0, 1, false);
+
+            var consumer = new QueueingBasicConsumer(channel);
+            channel.BasicConsume(RabbitConfiguration.QueueName, noAck, consumer);
+
+            Console.WriteLine(" [*] Waiting for messages...");
+
+            while (true)
+            {
+                var ea = (BasicDeliverEventArgs)consumer.Queue.Dequeue();
+
+                var body = ea.Body;
+
+                var message = Encoding.UTF8.GetString(body);
+
+                Console.WriteLine(" [x] Received {0}", message);
+
+                Thread.Sleep(5000);
+
+                channel.BasicAck(ea.DeliveryTag, false);
+            }
+        }
+    }
+}
---
+++ RabbitMQ_Demo2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace Demo2_Sender
+{
+    class Program
+    {
+        private readonly static dynamic RabbitConfiguration = new
+        {
+            Server = "192.168.100.86",
+            Username = "jfasenmyer",
+            Password = "password",
+            QueueName = "hello"
+        };
+
+        public static IModel BuildChannel(string hostName, string username, string password, int port = 5672)
+        {
+            ConnectionFactory factory = new ConnectionFactory { HostName = hostName, Port = port, UserName = username, Password = password };
+
+            IConnection connection = factory.CreateConnection();
+
+            return connection.CreateModel();
+        }
+
+        private static void Main(string[] args)
+        {
+            // Build a new channel to Rabbit.
+            IModel channel = BuildChannel(RabbitConfiguration.Server, RabbitConfiguration.Username, RabbitConfiguration.Password);
+
+            Console.WriteLine("Listening for input...");
+
+//            while (true)
+//            {
+                // Capture input from user
+//                string msg = Console.ReadLine();
+
+                // Declare a new queue called 'hello'
+                channel.QueueDeclare(RabbitConfiguration.QueueName, false, false, false, null);
+
+                for (int i = 0; i < 100000; i++)
+                {
+                    // Convert input to byte array
+//                    byte[] body = Encoding.UTF8.GetBytes(msg);
+                    byte[] body = Encoding.UTF8.GetBytes(i.ToString());
+
+                    // Write the byte array to the 'hello' queue.
+                    channel.BasicPublish("", RabbitConfiguration.QueueName, null, body);
+
+                    // Notify user that message was sent
+                    string notify = String.Format(" [x] Sent {0}", i);
+
+                    Console.WriteLine(notify);
+                }
+
+//            }
+        }
+    }
+}
